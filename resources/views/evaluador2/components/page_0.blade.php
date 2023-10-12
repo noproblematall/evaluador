130aa543095d53b816a781c2d0bab3c63bc18a53
noproblematall <aragon.jefferson89@gmail.com>
2023-10-12 14:29:50 -0400

    update img

--- resources/views/evaluador2/components/page_0.blade.php
+++ resources/views/evaluador2/components/page_0.blade.php
@@ -1,4 +1,6 @@
 <section id="page_0" class="page-content animate__animated animate__fadeIn ">
+    <img class='position-absolute w-100 bottom-0' src='/img/console+bottom.png' alt='bottom' />
+    <img class='position-absolute w-100 top-0' src='/img/console+top.png' alt='top' />
     <div class='box'>
         <img class='w-100 mb-5' src='https://z-eval.s3.amazonaws.com/texto+entrada.gif' alt='animation' />
     </div>
